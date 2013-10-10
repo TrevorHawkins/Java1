7daa856ccaa59eed4d88166356070c558c861412
TrevorHawkins <thawkins@student.neumont.edu>
2013-10-10 09:06:03 -0600

    Hello World project

---
+++ src/com/trevorhawkins/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.trevorhawkins.helloworld;
+
+public class HelloWorld {
+
+	public static void printMessage(){
+		System.out.println("Hello World");
+	}
+	public static void main(String[] args) {
+		printMessage();
+
+	}
+
+}
